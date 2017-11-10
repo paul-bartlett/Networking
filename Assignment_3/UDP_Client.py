cb57064521f4d03d8c6cbdbe4ac79f39a9011747
paul-bartlett <pbartle7@uwo.ca>
2017-11-10 17:18:15 -0500

    Finished most logic, handles ACK, checksums and sequence numbers where appropriate

--- Assignment_3/UDP_Client.py
+++ Assignment_3/UDP_Client.py
@@ -6,33 +6,42 @@ import hashlib
 
 UDP_IP = "192.168.2.25"
 UDP_PORT = 5005
-unpacker = struct.Struct('I I 32s')
+unpacker = struct.Struct('I 32s')
+SEQ = 0
 
 def udp_send(message):
+    global SEQ
     #Create the Checksum
-    values = (0,0,message)
-    UDP_Data = struct.Struct('I I 8s')
+    values = (SEQ,message)
+    UDP_Data = struct.Struct('I 8s')
     packed_data = UDP_Data.pack(*values)
     chksum =  bytes(hashlib.md5(packed_data).hexdigest(), encoding="UTF-8")
 
     #Build the UDP Packet
-    values = (0,0,message,chksum)
-    UDP_Packet_Data = struct.Struct('I I 8s 32s')
+    values = (SEQ,message,chksum)
+    UDP_Packet_Data = struct.Struct('I 8s 32s')
     UDP_Packet = UDP_Packet_Data.pack(*values)
+    SEQ = (SEQ + 1) % 2 #Set next SEQ number
 
     #Send the UDP Packet
-    sock = socket.socket(socket.AF_INET, # Internet
-        socket.SOCK_DGRAM) # UDP
-    sock.sendto(UDP_Packet, (UDP_IP, UDP_PORT))
-    print('sent message:',values)
-    data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+    flag = 1 #Terminate loop when successful
+    while flag: #Resends when NAK received or corrupted packet
+        sock = socket.socket(socket.AF_INET, # Internet
+                socket.SOCK_DGRAM) # UDP
+        sock.sendto(UDP_Packet, (UDP_IP, UDP_PORT))
+        print('Sent message:',values)
+        data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
 
-    #Receive ACK or NAK
-    UDP_Packet = unpacker.unpack(data)
-    if UDP_Packet[0] == 1:
-        print("received ACK:", UDP_Packet)
-    else:
-        print("received NAK, resending packet")
+        #Check if received an uncorrupted packet with ACK
+        UDP_Packet = unpacker.unpack(data)
+        if UDP_Packet[0] == 1 and UDP_Packet[1] == chksum:
+            print("Checksums match and received ACK:", UDP_Packet)
+            flag = 0
+        elif UDP_Packet[0] == 0:
+            print("Received NAK, resending packet:", UDP_Packet)
+        else:
+            print("Checksums do not match, resending packet:", UDP_Packet)
+        
 
 print("UDP target IP:", UDP_IP)
 print("UDP target port:", UDP_PORT)
--- Assignment_3/UDP_Server.py
+++ Assignment_3/UDP_Server.py
@@ -6,8 +6,8 @@ import hashlib
 
 UDP_IP = "192.168.2.25"
 UDP_PORT = 5005
-unpacker = struct.Struct('I I 8s 32s')
-
+unpacker = struct.Struct('I 8s 32s')
+SEQ = 0
 
 #Create the socket and listen
 sock = socket.socket(socket.AF_INET, # Internet
@@ -18,22 +18,26 @@ while True:
     #Receive Data
     data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
     UDP_Packet = unpacker.unpack(data)
-    print("received from:", addr)
-    print("received message:", UDP_Packet)
+    print("Received from:", addr)
+    print("Received message:", UDP_Packet)
     #Create the Checksum for comparison
-    values = (UDP_Packet[0],UDP_Packet[1],UDP_Packet[2])
-    packer = struct.Struct('I I 8s')
+    values = (UDP_Packet[0],UDP_Packet[1])
+    packer = struct.Struct('I 8s')
     packed_data = packer.pack(*values)
     chksum =  bytes(hashlib.md5(packed_data).hexdigest(), encoding="UTF-8")
     #Compare Checksums to test for corrupt data
-    UDP_Packet_Data = struct.Struct('I I 32s')
-    if UDP_Packet[3] == chksum:
-        print('Check Sums Match, Packet OK')
-        values = (1,0,chksum) # (ACK, SEQ, checksum)
-    else:
-        print('Checksums Do Not Match, Packet Corrupt')
-        values = (0,0,chksum) # (NAK, SEQ, checksum)
+    UDP_Packet_Data = struct.Struct('I 32s')
+    if UDP_Packet[2] == chksum and UDP_Packet[0] == SEQ:
+        print('Checksums and sequence numbers match, packet OK')
+        values = (1,chksum) # (ACK, checksum)
+    elif UDP_Packet[2] != chksum:
+        print('Checksums do not match, packet corrupt')
+        values = (0,chksum) # (NAK, checksum)
+    elif UDP_Packet[0] != SEQ:
+        print('Sequence numbers do not match, resending ACK')
+        values = (1,chksum) # (ACK, checksum)
 
     #Send ACK or NAK to client
     UDP_Packet = UDP_Packet_Data.pack(*values)
     sock.sendto(UDP_Packet, addr)
+    SEQ = (SEQ + 1) % 2 #Update SEQ to next base 2 value
