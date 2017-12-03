382b01301a2bd7452c17992cb2098658861537e7
paul-bartlett <pbartle7@uwo.ca>
2017-12-02 20:06:28 -0500

    Assignment 4 started

---
+++ Assignment_4/UDP_Client.py
@@ -0,0 +1,56 @@
+# Python3 UDP Client
+# udp_send will send a message to server, method calls are at the bottom
+import binascii
+import socket
+import struct
+import sys
+import hashlib
+
+UDP_IP = "127.0.0.1"
+UDP_PORT = 5005
+unpacker = struct.Struct('I 32s')
+SEQ = 0
+chksum = [None] * 2 #Remember last checksum for duplicate ACK
+
+def udp_send(message):
+    global SEQ, chksum
+
+    #Create the Checksum
+    values = (SEQ,message)
+    UDP_Data = struct.Struct('I 8s')
+    packed_data = UDP_Data.pack(*values)
+    chksum[SEQ] =  bytes(hashlib.md5(packed_data).hexdigest(), encoding="UTF-8")
+
+    #Build the UDP Packet
+    values = (SEQ,message,chksum[SEQ])
+    UDP_Packet_Data = struct.Struct('I 8s 32s')
+    UDP_Packet = UDP_Packet_Data.pack(*values)
+
+    #Send the UDP Packet
+    flag = 1 #Terminate loop when successful
+    while flag: #Resends when NAK received, duplicate ACK, or corrupted packet
+        sock = socket.socket(socket.AF_INET, # Internet
+                socket.SOCK_DGRAM) # UDP
+        sock.sendto(UDP_Packet, (UDP_IP, UDP_PORT))
+        print('Sent message:',values)
+        data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+
+        #Check if received an uncorrupted packet with ACK
+        UDP_Packet_recv = unpacker.unpack(data)
+        if chksum[(SEQ + 1) % 2] == chksum[SEQ]:
+            print("Duplicate ACK received, resending packet:", UDP_Packet_recv)
+        elif UDP_Packet_recv[0] == 1 and UDP_Packet_recv[1] == chksum[SEQ]:
+            print("Checksums match and received ACK:", UDP_Packet_recv)
+            flag = 0
+        elif UDP_Packet_recv[0] == 0:
+            print("Received NAK, resending packet:", UDP_Packet_recv)
+        else:
+            print("Checksums do not match, resending packet:", UDP_Packet_recv)   
+    SEQ = (SEQ + 1) % 2 #Set next SEQ number
+        
+
+print("UDP target IP:", UDP_IP)
+print("UDP target port:", UDP_PORT)
+udp_send(b'NCC-1701')
+udp_send(b'NCC-1664')
+udp_send(b'NCC-1017')
---
+++ Assignment_4/UDP_Server.py
@@ -0,0 +1,46 @@
+# Python3 UDP Server
+# No command line required, handles checksum errors and sequence number mismatches
+import binascii
+import socket
+import struct
+import sys
+import hashlib
+
+UDP_IP = "127.0.0.1"
+UDP_PORT = 5005
+unpacker = struct.Struct('I 8s 32s')
+SEQ = 0
+
+#Create the socket and listen
+sock = socket.socket(socket.AF_INET, # Internet
+                     socket.SOCK_DGRAM) # UDP
+sock.bind((UDP_IP, UDP_PORT))
+
+while True:
+    #Receive Data
+    data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+    UDP_Packet = unpacker.unpack(data)
+    print("Received from:", addr)
+    print("Received message:", UDP_Packet)
+    #Create the Checksum for comparison
+    values = (UDP_Packet[0],UDP_Packet[1])
+    packer = struct.Struct('I 8s')
+    packed_data = packer.pack(*values)
+    chksum =  bytes(hashlib.md5(packed_data).hexdigest(), encoding="UTF-8")
+    #Compare Checksums and sequence numbers to test for corrupt data and retransmissions
+    UDP_Packet_Data = struct.Struct('I 32s')
+    if UDP_Packet[2] == chksum and UDP_Packet[0] == SEQ:
+        print('Checksums and sequence numbers match, packet OK')
+        values = (1,chksum) # (ACK, checksum)
+    elif UDP_Packet[2] != chksum:
+        print('Checksums do not match, packet corrupt')
+        values = (0,chksum) # (NAK, checksum)
+    elif UDP_Packet[0] != SEQ:
+        print('Sequence numbers do not match, resending ACK')
+        values = (1,chksum) # (ACK, checksum)
+
+    #Send ACK or NAK to client
+    UDP_Packet = UDP_Packet_Data.pack(*values)
+    sock.sendto(UDP_Packet, addr)
+    print("Sent message:",values)
+    SEQ = (SEQ + 1) % 2 #Update SEQ to next base 2 value
