08fe46c45155070c36f6d21f23d2c827e8ff884e
paul-bartlett <pbartle7@uwo.ca>
2017-12-02 21:46:18 -0500

    Some timer parts implemented, sending multiple packets does not work

--- Assignment_4/UDP_Client.py
+++ Assignment_4/UDP_Client.py
@@ -5,14 +5,14 @@ import socket
 import struct
 import sys
 import hashlib
-
+from threading import Timer
 UDP_IP = "127.0.0.1"
 UDP_PORT = 5005
 unpacker = struct.Struct('I 32s')
 SEQ = 0
 chksum = [None] * 2 #Remember last checksum for duplicate ACK
 
-def udp_send(message):
+def create_packet(message):
     global SEQ, chksum
 
     #Create the Checksum
@@ -25,6 +25,11 @@ def udp_send(message):
     values = (SEQ,message,chksum[SEQ])
     UDP_Packet_Data = struct.Struct('I 8s 32s')
     UDP_Packet = UDP_Packet_Data.pack(*values)
+    print("Created packet:",values)
+    return UDP_Packet
+
+def send_packet(UDP_Packet):
+    global SEQ, chksum
 
     #Send the UDP Packet
     flag = 1 #Terminate loop when successful
@@ -32,8 +37,9 @@ def udp_send(message):
         sock = socket.socket(socket.AF_INET, # Internet
                 socket.SOCK_DGRAM) # UDP
         sock.sendto(UDP_Packet, (UDP_IP, UDP_PORT))
-        print('Sent message:',values)
+        print('Sent message')
         data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+         #Cancels timer if packet received before it runs out
 
         #Check if received an uncorrupted packet with ACK
         UDP_Packet_recv = unpacker.unpack(data)
@@ -47,10 +53,20 @@ def udp_send(message):
         else:
             print("Checksums do not match, resending packet:", UDP_Packet_recv)   
     SEQ = (SEQ + 1) % 2 #Set next SEQ number
-        
+
+def packet_timeout(values, UDP_Packet):
+    print("Timeout, resending packet")
+    send_packet(UDP_Packet)
 
 print("UDP target IP:", UDP_IP)
 print("UDP target port:", UDP_PORT)
-udp_send(b'NCC-1701')
-udp_send(b'NCC-1664')
-udp_send(b'NCC-1017')
+
+UDP_Packet = create_packet(b'NCC-1701')
+#Start timer before sending packet
+timer = Timer(0.009, send_packet, [UDP_Packet]).start()
+#UDP_Packet2 = create_packet(b'NCC-1017')
+#timer.start()
+#timer = Timer(1, send_packet, [UDP_Packet]).start()
+#UDP_Packet3 = create_packet(b'NCC-1017')
+#timer.start()
+#timer = Timer(1, send_packet, [UDP_Packet]).start()
